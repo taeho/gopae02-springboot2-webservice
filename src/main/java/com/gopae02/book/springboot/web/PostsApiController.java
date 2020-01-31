e500a542f4ec8a7c78b11a14159ff950831b331c
gopae02 <gopae02@gmail.com>
2020-01-31 16:52:13 +0900

    add/modify .igitignore file

--- src/main/java/com/gopae02/book/springboot/web/PostsApiController.java
+++ src/main/java/com/gopae02/book/springboot/web/PostsApiController.java
@@ -1,12 +1,11 @@
 package com.gopae02.book.springboot.web;
 
 import com.gopae02.book.springboot.service.PostsService;
+import com.gopae02.book.springboot.web.dto.PostsResponseDto;
 import com.gopae02.book.springboot.web.dto.PostsSaveRequestDto;
+import com.gopae02.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,4 +20,15 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
+    // 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    // 조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }

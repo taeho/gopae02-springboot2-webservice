488849c88b64d384bcb518222854333444d3ae15
gopae02 <gopae02@gmail.com>
2020-02-19 11:21:54 +0900

    add/modify .igitignore file # 로그인 사용자 이름 수정 userName -> loginUserName

--- src/main/java/com/gopae02/book/springboot/web/IndexController.java
+++ src/main/java/com/gopae02/book/springboot/web/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
     public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
         if (user != null) {
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("loginUserName", user.getName());
         }
         return "index";
     }

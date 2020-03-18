92ed05b937275b9543da312b2364159a48b4eea0
gopae02 <gopae02@gmail.com>
2020-03-18 22:35:08 +0900

    add/modify .igitignore file # 배포 자동화 구성

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-REPOSITORY=/home/ec2-user/app/step1
+
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=gopae02-springboot2-webservice
 
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git Pull"
-git pull
-
-echo "> 프로젝트 Build 시작"
-./gradlew build
-
-echo "> step1 디렉토리 이동"
-cd $REPOSITORY
-
 echo "> Build 파일 복사"
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
 
-echo "현재 구동중인 어플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl gopae02-springboot2-webservice | grep jar | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -29,12 +22,18 @@ else
 fi
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep .jar | tail -n 1)
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
